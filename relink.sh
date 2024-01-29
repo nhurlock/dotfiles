c81da502bad2249efc6c7a501484f89a92431cd2
nhurlock <nhurlock@gmail.com>
2024-01-29 09:19:47 -0500

    simple relink script for now

---
+++ relink.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.config
+
+# lazygit
+rm -rf ~/.config/lazygit
+ln -s "$(pwd)/lazygit" ~/.config/lazygit
+
+
+# nvim
+rm -rf ~/.config/nvim
+ln -s "$(pwd)/nvim" ~/.config/nvim
+
+
+# wezterm
+rm -rf ~/.config/wezterm
+ln -s "$(pwd)/wezterm" ~/.config/wezterm

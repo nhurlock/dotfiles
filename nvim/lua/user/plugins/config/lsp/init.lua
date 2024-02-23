d53f2452fc73b7bd880a67de506fc8d1e14bd8fb
nhurlock <nhurlock@gmail.com>
2024-02-23 18:57:49 -0500

    further move jdtls

--- nvim/lua/user/plugins/config/lsp/init.lua
+++ nvim/lua/user/plugins/config/lsp/init.lua
@@ -4,7 +4,8 @@ return {
     "williamboman/mason.nvim",           -- simple to use language server installer
     "williamboman/mason-lspconfig.nvim", -- simple to use language server installer
     "folke/neodev.nvim",                 -- neovim-specific lsp helpers
-    "pmizio/typescript-tools.nvim"       -- javascript/typescript-specific lsp helpers
+    "pmizio/typescript-tools.nvim",      -- javascript/typescript-specific lsp helpers
+    "mfussenegger/nvim-jdtls",           -- java-specific lsp helpers
   },
   config = function()
     require("neodev").setup()

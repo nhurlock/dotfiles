c4f4b561c0df8540bae0453dae3a78adf5f21926
nhurlock <nhurlock@gmail.com>
2024-06-01 17:07:26 -0400

    neodev deprecated moved to lazydev

--- nvim/lua/user/plugins/config/lsp/init.lua
+++ nvim/lua/user/plugins/config/lsp/init.lua
@@ -4,12 +4,20 @@ return {
   dependencies = {
     "williamboman/mason.nvim",           -- simple to use language server installer
     "williamboman/mason-lspconfig.nvim", -- simple to use language server installer
-    "folke/neodev.nvim",                 -- neovim-specific lsp helpers
-    "pmizio/typescript-tools.nvim",      -- javascript/typescript-specific lsp helpers
-    "mfussenegger/nvim-jdtls",           -- java-specific lsp helpers
+    {
+      "folke/lazydev.nvim",              -- lua-specific lsp helpers
+      ft = "lua",
+      opts = {
+        library = {
+          vim.env.LAZY .. "/luvit-meta/library",
+        },
+      },
+    },
+    { "Bilal2453/luvit-meta",         lazy = true },               -- optional `vim.uv` typings
+    { "notomo/typescript-tools.nvim", branch = "fix-deprecated" }, -- javascript/typescript-specific lsp helpers
+    "mfussenegger/nvim-jdtls",                                     -- java-specific lsp helpers
   },
   config = function()
-    require("neodev").setup()
     require("lspconfig")
     require("user.plugins.config.lsp.mason")
     require("user.plugins.config.lsp.handlers").setup()

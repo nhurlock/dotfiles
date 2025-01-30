5fa9ad3b251889bc865a8d1d8eded1e2fc453937
nhurlock <nhurlock@gmail.com>
2025-01-30 18:43:32 -0500

    swap local ai comp to llama.vim, plugin updates

---
+++ nvim/lua/user/plugins/config/llama.lua
@@ -0,0 +1,19 @@
+-- llama-server \
+--     -hf ggml-org/Qwen2.5-Coder-7B-Q8_0-GGUF \
+--     --port 8012 -ngl 99 -fa -ub 1024 -b 1024 \
+--     --ctx-size 0 --cache-reuse 256
+
+---@type LazyPluginSpec
+return {
+  "ggml-org/llama.vim",
+  enabled = vim.env.USER == "nhurlock",
+  init = function()
+    vim.g.llama_config = {
+      show_info = 0
+    }
+  end,
+  config = function()
+    vim.api.nvim_set_hl(0, "llama_hl_hint", { link = "Comment", force = true })
+    vim.api.nvim_set_hl(0, "llama_hl_info", { link = "Comment", force = true })
+  end
+}

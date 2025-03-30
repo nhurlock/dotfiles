6e1ec844df584584dbe3595f06d155767d9e7e54
nhurlock <nhurlock@gmail.com>
2025-03-30 13:08:10 -0400

    vtsls inlay hints for toggling

--- nvim/lua/user/plugins/config/lsp/settings/vtsls.lua
+++ nvim/lua/user/plugins/config/lsp/settings/vtsls.lua
@@ -1,3 +1,14 @@
 return {
-  settings = {},
+  settings = {
+    typescript = {
+      inlayHints = {
+        parameterNames = { enabled = 'literals' },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        enumMemberValues = { enabled = true },
+      },
+    },
+  },
 }

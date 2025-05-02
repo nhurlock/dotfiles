c34dbca02e7de21c6f019ffe91e24a81ef3cf6ab
nhurlock <nhurlock@gmail.com>
2025-05-02 19:54:12 -0400

    qmk.nvim update to support both keyboards

--- nvim/lua/user/plugins/config/qmk.lua
+++ nvim/lua/user/plugins/config/qmk.lua
@@ -3,19 +3,50 @@
 ---@type LazyPluginSpec
 return {
   'codethread/qmk.nvim',
-  ---@type qmk.UserConfig
-  opts = {
-    name = 'LAYOUT',
-    variant = 'qmk',
-    comment_preview = {
-      keymap_overrides = {},
-    },
-    layout = {
-      'x x x x x x x _ x x x x x x x',
-      'x x x x x x x _ x x x x x x x',
-      'x x x x x x x _ x x x x x x x',
-      'x x x x x x x _ x x x x x x x',
-      '_ _ _ x x x x _ x x x x _ _ _',
-    },
-  },
+  init = function()
+    local group = vim.api.nvim_create_augroup('qmk', {})
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      desc = 'ai03 Altair',
+      group = group,
+      pattern = '*ai03/altair/*/keymap.c',
+      callback = function()
+        require('qmk').setup({
+          name = 'LAYOUT',
+          variant = 'qmk',
+          comment_preview = {
+            keymap_overrides = {},
+          },
+          layout = {
+            'x x x x x x x _ x x x x x x x',
+            'x x x x x x x _ x x x x x x x',
+            'x x x x x x x _ x x x x x x x',
+            'x x x x x x x _ x x x x x x x',
+            '_ _ _ x x x x _ x x x x _ _ _',
+          },
+        })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      desc = 'Corne',
+      group = group,
+      pattern = '*corne.keymap',
+      callback = function()
+        require('qmk').setup({
+          name = 'corne',
+          variant = 'zmk',
+          comment_preview = {
+            keymap_overrides = {},
+          },
+          layout = {
+            'x x x x x x _ x x x x x x',
+            'x x x x x x _ x x x x x x',
+            'x x x x x x _ x x x x x x',
+            '_ _ _ x x x _ x x x _ _ _',
+          },
+        })
+      end,
+    })
+  end,
 }

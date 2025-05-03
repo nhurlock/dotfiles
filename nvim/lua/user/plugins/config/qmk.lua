99df715c2f489d528cc9eafdc664b766cae72dd5
nhurlock <nhurlock@gmail.com>
2025-05-03 15:56:01 -0400

    5 col, plugin updates

--- nvim/lua/user/plugins/config/qmk.lua
+++ nvim/lua/user/plugins/config/qmk.lua
@@ -40,10 +40,10 @@ return {
             keymap_overrides = {},
           },
           layout = {
-            'x x x x x x _ x x x x x x',
-            'x x x x x x _ x x x x x x',
-            'x x x x x x _ x x x x x x',
-            '_ _ _ x x x _ x x x _ _ _',
+            'x x x x x _ x x x x x',
+            'x x x x x _ x x x x x',
+            'x x x x x _ x x x x x',
+            '_ _ x x x _ x x x _ _',
           },
         })
       end,
